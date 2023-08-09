a4bf31ab8e4d7a78d7c4b74f4ffefe9f2d470d07
Daniil <nektos13@yandex.ru>
2023-08-09 22:00:33 +0300

    написал CRUD для заказов

--- src/main/java/com/daniil/comicshop/service/OrderService.java
+++ src/main/java/com/daniil/comicshop/service/OrderService.java
@@ -1,7 +1,5 @@
 package com.daniil.comicshop.service;
 
-import com.daniil.comicshop.dto.request.ComicIdsRequest;
-import com.daniil.comicshop.entity.Artist;
 import com.daniil.comicshop.entity.Order;
 
 import java.util.List;
@@ -12,6 +10,6 @@ public interface OrderService {
     Order add(Order order);
     Optional<Order> getById(UUID uuid);
     List<Order> getAll();
-    Artist change(Order order);
+    Order change(Order order);
     Optional<Order> delete(UUID uuid);
 }
--- src/main/java/com/daniil/comicshop/service/impl/OrderServiceImpl.java
+++ src/main/java/com/daniil/comicshop/service/impl/OrderServiceImpl.java
@@ -1,5 +1,50 @@
 package com.daniil.comicshop.service.impl;
 
 
-public class OrderServiceImpl {
+import com.daniil.comicshop.entity.Client;
+import com.daniil.comicshop.entity.Order;
+import com.daniil.comicshop.repository.OrderRepository;
+import com.daniil.comicshop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository repository;
+
+    @Override
+    public Order add(Order order) {
+        order.setUuid(UUID.randomUUID());
+        return repository.save(order);
+    }
+
+    @Override
+    public Optional<Order> getById(UUID uuid) {
+        return repository.findById(uuid);
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Order change(Order order) {
+        if (repository.findById(order.getUuid()).isPresent()){
+            return repository.save(order);}
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public Optional<Order> delete(UUID uuid) {
+        Optional<Order> order = repository.findById(uuid);
+        repository.deleteById(uuid);
+        return order;
+    }
 }
